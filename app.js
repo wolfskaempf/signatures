5460d564dedf71c15b25520c18dc863f634d40b6
wolfskaempf <tom@wolfskaempf.de>
2016-04-18 19:10:57 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        fname: 'Max',
+        surname: 'Mustermann',
+        posde: 'Projektmitarbeiter',
+        posen: 'Project Assistant',
+        sessionde: '26. Nationale Auswahlsitzung Lübeck 2016',
+        sessionen: '26th National Selection Conference Lübeck 2016',
+        mobilenumber: '+49 (0) 123 456 78 90',
+        email: 'max.mustermann@eyp.de',
+    }
+});
