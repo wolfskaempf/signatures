977100d94c6f7f4e893676c373bbd47f09e0cf20
Lukas Harkotte <88850343+LukasHark@users.noreply.github.com>
2021-08-12 20:28:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        fname: 'Max',
-        surname: 'Mustermann',
-        posde: 'Projektmitarbeiter',
+        fname: 'Marie',
+        surname: 'Musterfrau',
+        posde: 'Projektmitarbeiterin',
         posen: 'Project Assistant',
         sessionde: 'Nationale Auswahlsitzung Bad Segeberg 2022',
         sessionen: 'National Selection Conference Bad Segeberg 2022',
         mobilenumber: '+49 (0) 123 456 78 90',
-        email: 'max.mustermann@eyp.de',
-        genderde: 'er/ihm',
-        genderen: 'he/him',
+        email: 'marie.musterfrau@eyp.de',
+        genderde: 'sie/ihr',
+        genderen: 'she/her',
     }
 });
